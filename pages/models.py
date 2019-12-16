91dd3e803b297501def5a436caccb0d64a66e197
Yang Shuqi <shu7yang@gmail.com>
2019-12-16 20:27:13 +0800

    modify models

--- pages/models.py
+++ pages/models.py
@@ -1,26 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    type = models.CharField(max_length=50, primary_key=True)
+
+    def __str__(self):
+        return self.type
+
+
 class Data(models.Model):
     question_text = models.CharField(max_length=200)
     answer_text = models.TextField()
-    type_text = models.CharField(max_length=100)
+    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "Q: {}, A: {}, T: {}".format(self.question_text, self.answer_text, self.type)
 
     @classmethod
     def create(cls, qns, ans, type):
-        data = cls(question_text=qns, answer_text=ans,type_text=type)
-        # do something with the book
+        data = cls(question_text=qns, answer_text=ans,type=type)
         return data
+
 
 class VotingData(models.Model):
     question_text = models.CharField(max_length=200)
-    answer_text_1 = models.TextField()
-    answer_text_2 = models.TextField()
-    answer_text_3 = models.TextField()
-    type_text = models.CharField(max_length=100)
+    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "Q: {}, T: {}".format(self.question_text, self.type)
 
     @classmethod
-    def create(cls, qns, ans1, ans2, ans3, type):
-        data = cls(question_text=qns, answer_text_1=ans1, answer_text_2=ans2, answer_text_3=ans3,type_text=type)
-        # do something with the book
+    def create(cls, qns, type):
+        data = cls(question_text=qns, type=type)
+        return data
+
+
+class Choice(models.Model):
+    data = models.ForeignKey(VotingData, models.CASCADE)
+    answer = models.TextField()
+    num_votes = models.IntegerField()
+
+    def __str__(self):
+        return "ID: {}, A: {}, N: {}".format(self.data, self.answer, self.num_votes)
+
+    @classmethod
+    def create(cls, data, ans, num_votes):
+        data = cls(data=data, answer=ans, num_votes=num_votes)
         return data
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+    def test_model(self):
+        test_type1, created = Type.objects.update_or_create(type="T1")
+        test_type2, created = Type.objects.update_or_create(type="T2")
+        print(Type.objects.all())
+
+        test_voting_1, created = VotingData.objects.update_or_create(question_text="Vote1", type=test_type1)
+        test_voting_2, created = VotingData.objects.update_or_create(question_text="Vote2", type=test_type2)
+        print(VotingData.objects.all())
+
+        test_validating_1, created = Data.objects.update_or_create(question_text="Val1", answer_text="A1", type=test_type2)
+        test_validating_2, created = Data.objects.update_or_create(question_text="Val2", answer_text="A1")
+        print(Data.objects.all())
+
+        test_choice_11, created = Choice.objects.update_or_create(data=test_voting_1, answer="AC1", num_votes=1)
+        test_choice_12, created = Choice.objects.update_or_create(data=test_voting_1, answer="AC2", num_votes=0)
+        print(Choice.objects.all())
+
+        test_choice_12.update(num_votes=6)
+        print(Choice.objects.all())
