5de05f47b78cbe055ccf3c8062d1744677a23026
Yang Shuqi <shu7yang@gmail.com>
2019-12-28 18:50:47 +0800

    add error-msg for registration

--- pages/views/admin.py
+++ pages/views/admin.py
@@ -9,11 +9,17 @@ import csv
 import time
 import datetime
 import json
-
+from django.contrib import messages
 
 ADMIN_DIR = 'pages/admin'
 USER_DIR = 'pages/user'
 AUTH_DIR = 'authentication'
+
+MSG_SUCCESS = "User registration succeed"
+MSG_FAIL_EMAIL = "Email {} has been used"
+MSG_FAIL_CERTI = "Certificate {} has been used"
+MSG_FAIL_FILL = "Please fill in all fields."
+
 
 @login_required
 def index(request):
@@ -21,12 +27,27 @@ def index(request):
     if user is not None:
         if user.is_superuser:
             template = loader.get_template('{}/admin.html'.format(ADMIN_DIR))
+
             context = {
                 'specs': Specialization.objects.all(),
                 'groups': CustomGroup.objects.all(),
-                'login_user': user,
+                'login_user': request.user,
             }
-            return HttpResponse(template.render(context=context))
+
+            obj = request.session.pop('obj', False)
+            if obj:
+                context['obj'] = obj
+
+            success = request.session.pop('success', False)
+
+            if success:
+                messages.success(request, success)
+
+            fail = request.session.pop('error', False)
+            if fail:
+                messages.add_message(request, messages.ERROR, message=fail, extra_tags="danger")
+
+            return HttpResponse(template.render(context=context, request=request))
         else:
             return HttpResponseRedirect('/')
     else:
@@ -42,20 +63,37 @@ def index(request):
 @csrf_exempt
 def add_user(request):
     if request.method == "POST":
-        print(request.POST)
         username = request.POST["username"]
         certificate = request.POST["certificate"]
         email = request.POST["email"]
-        if not request.POST["group"]:
-            group = CustomGroup.objects.get(id=request.POST["group"])
-        else:
-            group = None
-        if CustomUser.objects.get(email=email):
-            return HttpResponse("Email {} has been used".format(email))
+        group = CustomGroup.objects.get(id=request.POST["group"])
+
+        if not username or not certificate or not email or not group:
+            request.session['error'] = MSG_FAIL_FILL
+            request.session['obj'] = request.POST
+            return HttpResponseRedirect('/')
+
+        user = None
+        try:
+            user = CustomUser.objects.get(email=email)
+            request.session['error'] = MSG_FAIL_EMAIL.format(email)
+        except Exception:
+            pass
+
+        try:
+            user = CustomUser.objects.get(certificate=certificate)
+            request.session['error'] = MSG_FAIL_CERTI.format(certificate)
+        except Exception:
+            pass
+
+        if user:
+            request.session['obj'] = request.POST
+            return HttpResponseRedirect('/')
         else:
             CustomUser.objects.create_user(email=email, certificate=certificate, username=username,
                                            group=group, password="123456")
-        return HttpResponseRedirect('/')
+            request.session['success'] = MSG_SUCCESS
+            return HttpResponseRedirect('/')
     else:
         return HttpResponse("Request method is not allowed.")
 
